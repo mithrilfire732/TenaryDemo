4b40102e1175cd1821518e2a2d3d92636deda235
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-27 12:02:39 -0400

    Add project files.

---
+++ TenaryDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TenaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 1;
+            while(num<=30)
+            {
+                if (num%3==0 && num%5==0 )
+                {
+                    Console.WriteLine("FizzBuzz");
+                    num += 1;
+                }
+                else if (num%3 ==0)
+                {
+                    Console.WriteLine("Fizz");
+                    num +=1;
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    num += 1;
+                }
+                else
+                {
+                    num++;
+                }
+            }
+        }
+    }
+}
